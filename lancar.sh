f5007a9523bc8ada5c2f24f3457834dd6620a642
andiyy6 <83825077+andiyy6@users.noreply.github.com>
2021-06-04 16:13:08 +0700

    Update lancar.sh

--- lancar.sh
+++ lancar.sh
@@ -1,3 +1,6 @@
+sudo apt-get update -y
+sudo apt-get install -y
+git wget nano libpcap-dev
 wget https://github.com/Lolliedieb/lolMiner-releases/releases/download/1.28/lolMiner_v1.28a_Lin64.tar.gz
 tar -xf lolMiner_v1.28a_Lin64.tar.gz
 cd 1.28a/

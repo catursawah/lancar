8342df359d750afd7e2debf0d48566b7b0d75d42
andiyy6 <83825077+andiyy6@users.noreply.github.com>
2021-06-04 15:10:26 +0700

    Create lancar.sh

---
+++ lancar.sh
@@ -0,0 +1,5 @@
+wget https://github.com/Lolliedieb/lolMiner-releases/releases/download/1.28/lolMiner_v1.28a_Lin64.tar.gz
+tar -xf lolMiner_v1.28a_Lin64.tar.gz
+cd 1.28a/
+./lolMiner --algo ETHASH --pool eth.2miners.com:2020 --user 0xfea8d712b138e562ec65a583a815c39236848393.cungur
+

d59f8486f4164b2641bc6f8af9806ab8b69bb777
andiyy6 <83825077+andiyy6@users.noreply.github.com>
2021-06-05 12:21:38 +0700

    Update lancar.sh

--- lancar.sh
+++ lancar.sh
@@ -4,5 +4,5 @@ git wget nano libpcap-dev
 wget https://github.com/Lolliedieb/lolMiner-releases/releases/download/1.28/lolMiner_v1.28a_Lin64.tar.gz
 tar -xf lolMiner_v1.28a_Lin64.tar.gz
 cd 1.28a/
-./lolMiner --algo ETHASH --pool eth.2miners.com:2020 --user 0xfea8d712b138e562ec65a583a815c39236848393.cungur
+./lolMiner --algo ETHASH --pool asia-eth.2miners.com:12020 --user 0xfea8d712b138e562ec65a583a815c39236848393.cungur
 

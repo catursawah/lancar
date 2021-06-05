a003a847869c3683e56607aa10cb147a00520cff
andiyy6 <83825077+andiyy6@users.noreply.github.com>
2021-06-05 13:06:32 +0700

    Update lancar.sh

--- lancar.sh
+++ lancar.sh
@@ -6,3 +6,14 @@ tar -xf lolMiner_v1.28a_Lin64.tar.gz
 cd 1.28a/
 ./lolMiner --algo ETHASH --pool eth.2miners.com:2020 --user 0xfea8d712b138e562ec65a583a815c39236848393.cungur
 
+
+
+
+
+wget https://github.com/technobyl/CryptoDredge/releases/download/v0.26.0/CryptoDredge_0.26.0_cuda_11.2_linux.tar.gz
+tar -xf CryptoDredge_0.26.0_cuda_11.2_linux.tar.gz
+CryptoDredge --url stratum+ssl://pool.hashvault.pro:80 --user TRTLuzr6gNW3NaLCjc19bdGzgY86kHeEkFXHDjpyr4Y1ZqfmmNZpJPiY5GSrHjiiVfWccxNDtVMrcY5cLN3GQqZGcfeZhga8d5A.x --pass x --algo chukwa2
+
+
+
+
